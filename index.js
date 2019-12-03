51f32c73c2f34dffd4b87afc8bd2a0b0953215ce
shikhar-priyadarshi-vst-au4 <54432582+shikhar-priyadarshi-vst-au4@users.noreply.github.com>
2019-12-03 15:56:00 +0530

    nearly done

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express=require('express');
 const morgan=require('morgan');
-const app=express();
 const exphbs=require('express-handlebars');
-const hbs=exphbs.create({
+const app=express();
+app.engine('.hbs', exphbs({extname: '.hbs'}));
+app.set('view engine', '.hbs');
+
+/*const hbs=exphbs.create({
     extname:'.hbs'
-       });
+       });*/
 
 const PORT=process.env.PORT||'3000';
 const db=require('./models/index');
@@ -34,8 +37,8 @@ app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
 app.use('/public',express.static('public'));
-app.engine('.hbs',hbs.engine);
-app.set('view engine','.hbs');
+//app.engine('.hbs',hbs.engine);
+//app.set('view engine','.hbs');
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(validator());
