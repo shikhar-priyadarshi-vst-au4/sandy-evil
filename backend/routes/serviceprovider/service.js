f9dbb28d060309a051ccb2c2dd326d612b57897f
shikhar-priyadarshi-vst-au4 <officialshikharid@gmail.com>
2020-04-22 03:16:48 +0530

    Work in progress

--- backend/routes/serviceprovider/service.js
+++ backend/routes/serviceprovider/service.js
@@ -2,8 +2,13 @@ const router = require('express').Router();
 
 const { isAuthorised } = require('../../controllers/serviceprovider/worker');
 
-router.post('/add-services', isAuthorised, ( req, res) => {
-    res.send('add services');
+const { inject, render } = require('../../controllers/serviceprovider/service')
+
+router.post('/add/services', isAuthorised, ( req, res) => {
+    console.log('Work in progress');
 })
 
+// to use inject services in DB
+router.post('/inject', inject);
+router.get('/render', render);
 module.exports = router;
