f8d90aeddf27bdbb0eb5752c46f543b3c70ef973
shikhar-priyadarshi-vst-au4 <officialshikharid@gmail.com>
2020-04-24 19:16:47 +0530

    13th commit : working

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 const worker = require('./serviceprovider/worker');
 const service = require('./serviceprovider/service');
 const consumer = require('./consumer/profile');
+
 //worker or service-provider route
 router.use('/worker', worker);
 router.use('/services', service );
+
 //consumer route
 router.use('/user', consumer);
 
